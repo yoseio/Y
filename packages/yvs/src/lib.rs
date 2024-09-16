94da42e65764ee0d0db0233d334a7bc5926513af
yoseio <98276492+yoseio@users.noreply.github.com>
2024-09-16 07:08:37 +0000

    feat(yvs): implement Yvs

--- packages/yvs/src/lib.rs
+++ packages/yvs/src/lib.rs
@@ -1,15 +1,6 @@
-use wasm_bindgen::prelude::*;
+mod yvs;
+pub use yvs::*;
 
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, yvs!");
-}
